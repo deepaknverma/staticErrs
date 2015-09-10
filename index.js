b93ca1db89fbe3d4ba7f0926204d1b0cba95653e
Deepak Verma <deepak.verma@hometrack.com>
2015-09-11 08:50:16 +1000

    update index.js

--- index.js
+++ index.js
@@ -3,17 +3,9 @@
 * @Author: Deepak Verma
 * @Date:   2015-08-25 09:16:56
 * @Last Modified by:   Deepak Verma
-* @Last Modified time: 2015-09-11 06:50:56
+* @Last Modified time: 2015-09-11 08:49:48
 */
 
 // TODO: work in progress to integrate reporting and logging
 
-var staticErrs = require("./lib/staticerrs.js");
-
-staticErrs('./lib/', function(err, data){
-	if(err){
-		console.log(err);
-	}
-	
-	console.log(data);
-});
+module.exports = require("./lib/staticerrs.js");
